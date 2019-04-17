c61811d029398629a907ca111974e7bb5d3e9c59
Weston Straw <weston.straw@gmail.com>
2019-04-17 17:16:30 -0500

    Create UserFactory.java

---
+++ CurrentApp/src/main/java/UserFactory.java
@@ -0,0 +1,14 @@
+public class UserFactory {
+    public UserModel getUser(String userType, String[] data) {
+        if(userType == null) {
+            return null;
+        }
+
+        if(userType.equalsIgnoreCase("0")) {
+            return new UserModel(data[0], data[1], data[2], data[3], data[4], data[5]);
+        }
+        //else if(userType.equalsIgnoreCase("Rep"){ return repModel }
+        //else if(userType.equalsIgnoreCase("Admin"){ return adminModel }
+        return null;
+    }
+}
