65d32ecf02d54b982edfd48e9e8edd7eff16b99c
Mark Du <mark_du1@baylor.edu>
2019-04-18 20:08:25 -0500

    Worked on CarModel

---
+++ CurrentApp/src/main/java/models/CarModel.java
@@ -0,0 +1,14 @@
+package models;
+
+public class CarModel {
+    private int price;
+    private int price;
+    private int price;
+    private int price;
+    private int price;
+    private String make;
+    private String make;
+    private String make;
+    private String make;
+
+}
